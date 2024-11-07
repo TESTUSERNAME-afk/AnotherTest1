5630412bb82e8cb51c4b95f4bbeb6e599c05a16e
TESTUSERNAME-afk <danielcampos1145@gmail.com>
2024-11-07 22:58:52 +0000

    Another Test

--- Program.java
+++ Program.java
@@ -1,5 +1,10 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("It worked");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello Dad");
+            System.out.println("Hello Mom");
+            System.out.println("Hello Brother");
+            System.out.println("Hello World");
+        }
     }
 }
